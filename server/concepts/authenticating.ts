990be0f1dc34884ee85da3848de9dfbd2f8cb782
jmoler2 <114830361+jmoler2@users.noreply.github.com>
2024-09-26 08:53:52 -0400

    done this

--- server/concepts/authenticating.ts
+++ server/concepts/authenticating.ts
@@ -33,10 +33,15 @@ export default class AuthenticatingConcept {
   }
 
   async getUserById(_id: ObjectId) {
+    const filter = {_id: {$eq: _id}}
     // TODO 1: implement this operation
     //  - use this.users.readOne(..)
     //  - don't include the password (we've provided a helper function you can use!)
-    throw new Error("Not implemented!");
+    const user = await this.users.readOne({_id});
+    if (user === null) {
+      throw new Error("blegh");
+    }
+    return this.redactPassword(user)
   }
 
   async getUsers(username?: string) {
@@ -58,7 +63,9 @@ export default class AuthenticatingConcept {
     // TODO 2: implement this operation
     //  - use this.users.partialUpdateOne(..)
     //  - maintain the invariant that usernames are unique (we've provided a helper function!)
-    throw new Error("Not implemented!");
+    this.assertUsernameUnique(username);
+    const filter = {_id}
+    this.users.partialUpdateOne(filter, {"username": username});
   }
 
   async delete(_id: ObjectId) {
